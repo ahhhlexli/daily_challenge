b11d1db4b945dbdf871058a50afcc376f165bfd1
ahhhlexli <alexli0605@gmail.com>
2021-03-02 09:39:05 +0800

    Day 30

---
+++ day30.py
@@ -0,0 +1,26 @@
+"""Someone has attempted to censor my strings by replacing every vowel with a *, l*k* th*s. Luckily, I've been able to find the vowels that were removed.
+
+Given a censored string and a string of the censored vowels, return the original uncensored string.
+
+Example
+uncensor("Wh*r* d*d my v*w*ls g*?", "eeioeo") ➞ "Where did my vowels go?"
+
+uncensor("abcd", "") ➞ "abcd"
+
+uncensor("*PP*RC*S*", "UEAE") ➞ "UPPERCASE"
+"""
+
+def uncensor(string, vowels):
+
+    current = 0
+    string = list(string)
+    for i in range(len(string)):
+        if string[i] == '*':
+            string[i] = vowels[current]
+            current += 1
+
+    return ''.join(string)
+
+print(uncensor("Wh*r* d*d my v*w*ls g*?", "eeioeo"))
+print(uncensor("abcd", ""))
+print(uncensor("*PP*RC*S*", "UEAE"))
